0f1fea790c47730d1aebb405985cf449455057da
Andrew <zatop96@mail.ru>
2019-09-19 21:53:28 +0300

    New file with an example of py

---
+++ Untitled.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import math
+a = 0
+b = 0
+c = 0
+while a < 10:
+    b = b + 5
+    c = c + 10
+    a = a + 1
+    print("a =", a, "b =", b, "c =", c)
+
+
+
+
+
